ba17969207b5780dec5e02c9e627bcee2b4423b4
megitacaila07-crypto <megitacaila07@gmail.com>
2026-09-20 10:11:57 +0700

    ini gitaa bukan github

---
+++ 260404100078_Megita Caila Amru/soal.py
@@ -0,0 +1,18 @@
+# nama_peminjam = input()
+# no_anggota = 5
+# jumlah_buku = 7
+# status = True
+# durasi = 6
+
+# print(nama_peminjam)
+
+jumlah_tiket = "5"
+harga_tiket = 25000
+total_sementara = jumlah_tiket*2
+
+print("Total sementara:", total_sementara)
+print("Harga tiket:", harga_tiket)
+
+jumlah_tiket = int(jumlah_tiket)
+total_akhir =jumlah_tiket*harga_tiket
+print("Total akhir:", total_akhir)
